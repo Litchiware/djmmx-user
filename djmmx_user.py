56ec11b1d3103df42ddd4d5b13a2067298fe6f4a
李亮民 <llm@huantengsmart.com>
2015-10-28 22:13:20 +0800

    design login pages

--- djmmx_user.py
+++ djmmx_user.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template  
   
 app = Flask(__name__)  
-app.debug = True  
  
 @app.route('/')  
 def index():  
@@ -9,4 +8,4 @@ def index():
   
       
 if __name__ == '__main__':  
-        app.run()
+    app.run(host='0.0.0.0')
